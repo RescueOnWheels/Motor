9c540974d13dbb0707fd482702caeb7a32b4fe7c
Martijn Vegter <martijn@martijnvegter.com>
2018-09-11 17:07:10 +0200

    Chuck Norris Emailed Me This Patch... I'm Not Going To Question It.
    Added ESLint and fixed some things.

--- index.js
+++ index.js
@@ -2,56 +2,55 @@ function Motor() {
 
 }
 
-var i2c = require('i2c');
-var address = 0x32;
-var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+const I2C = require('i2c');
+
+const address = 0x32;
+const wire = new I2C(address, { device: '/dev/i2c-1' }); // point to your i2c address, debug provides REPL interface
 
 function wireWrite(side) {
-    wire.write(side, (err) => {
-        if (err) {
-            console.log(err);    
-        }
-    });
+  wire.write(side, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 // Rotating to the left
-Motor.prototype.Left = function() {
-    var left = [7,3,0xa5,1,3,0xa5,2]; 
-    wireWrite(left);
-}
+Motor.prototype.Left = () => {
+  const left = [7, 3, 0xa5, 1, 3, 0xa5, 2];
+  wireWrite(left);
+};
 
 // Rotating to the right
-Motor.prototype.Right = function() {
-    var right = [7,3,0xa5,2,3,0xa5,1]; 
-    wireWrite(right);
-}
+Motor.prototype.Right = () => {
+  const right = [7, 3, 0xa5, 2, 3, 0xa5, 1];
+  wireWrite(right);
+};
 
 // Driving to the left
-Motor.prototype.LeftDrive = function() {
-    var left = [7,3,0x52,2,3,0xa5,2]; 
-    wireWrite(left);
-}
+Motor.prototype.LeftDrive = () => {
+  const left = [7, 3, 0x52, 2, 3, 0xa5, 2];
+  wireWrite(left);
+};
 
 // Forwards
-Motor.prototype.Forward = function(direction) {
-    direction += 100;
-
-    var left = Math.round(255 - ((255 / 200) * direction));
-    var right = 255 - left;
-    var forward = [7,3,left,2,3,right,2]; 
-    wireWrite(forward);
-}
+Motor.prototype.Forward = (direction) => {
+  const left = Math.round(255 - ((255 / 200) * (direction + 100)));
+  const right = 255 - left;
+  const forward = [7, 3, left, 2, 3, right, 2];
+  wireWrite(forward);
+};
 
 // Full power forwards
-Motor.prototype.Boost = function() {
-    var boostForward = [7,3,0xFF,2,3,0xFF,2]; 
-    wireWrite(boostForward);
-}
+Motor.prototype.Boost = () => {
+  const boostForward = [7, 3, 0xFF, 2, 3, 0xFF, 2];
+  wireWrite(boostForward);
+};
 
 // Stopping
-Motor.prototype.Stop = function() {
-    var stopping = [7,0,0,0,0,0,0]; 
-    wireWrite(stopping);
-}
+Motor.prototype.Stop = () => {
+  const stopping = [7, 0, 0, 0, 0, 0, 0];
+  wireWrite(stopping);
+};
 
 module.exports = Motor;
