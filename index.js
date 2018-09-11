5f33a3c2456a94c1164a4fb1054a5cb851284ba5
Christiaan <christiaanvanarum2@gmail.com>
2018-09-11 15:22:24 +0200

    happy monday _ bleh _
    Translated Dutch > English

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var i2c = require('i2c');
 var address = 0x32;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
-// Naar links draaien
+// Rotating to the left
 Motor.prototype.Left = function() {
     var Left = [7,3,0xa5,1,3,0xa5,2]; 
     wire.write(Left, function(err) {
@@ -15,7 +15,7 @@ Motor.prototype.Left = function() {
 
 }
 
-// Naar rechts draaien
+// Rotating to the right
 Motor.prototype.Right = function() {
     var Right = [7,3,0xa5,2,3,0xa5,1]; 
     wire.write(Right, function(err) {
@@ -24,7 +24,7 @@ Motor.prototype.Right = function() {
 
 }
 
-// Naar links rijden
+// Driving to the left
 Motor.prototype.LeftDrive = function() {
     var Left = [7,3,0x52,2,3,0xa5,2]; 
     wire.write(Left, function(err) {
@@ -33,20 +33,20 @@ Motor.prototype.LeftDrive = function() {
 
 }
 
-// Naar voren
-Motor.prototype.Forward = function(direction) {
-    direction += 100;
+// Forwards
+Motor.prototype.Forward = function(Direction) {
+    Direction += 100;
 
-    var links = Math.round(255 - ((255 / 200) * direction));
-    var rechts = 255 - links;
-    var MForward = [7,3,links,2,3,rechts,2]; 
-    wire.write(MForward, function(err) {
+    var Left = Math.round(255 - ((255 / 200) * Direction));
+    var Right = 255 - Left;
+    var Forward = [7,3,Left,2,3,Right,2]; 
+    wire.write(Forward, function(err) {
         console.log(err);
     });   
     
 }
 
-// Vol gas naar voren
+// Full power forwards
 Motor.prototype.Boost = function() {
     var BoostForward = [7,3,0xFF,2,3,0xFF,2]; 
     wire.write(BoostForward, function(err) {
@@ -55,7 +55,7 @@ Motor.prototype.Boost = function() {
     
 }
 
-// Stoppen
+// Stopping
 Motor.prototype.Stop = function() {
     var Stopping = [7,0,0,0,0,0,0]; 
     wire.write(Stopping, function(err) {
@@ -63,6 +63,5 @@ Motor.prototype.Stop = function() {
     });      
 
 }
-
 
 module.exports = Motor;
