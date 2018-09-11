cb616978da9ad1e6ef34c36ef9980392d77b39c7
Christiaan <christiaanvanarum2@gmail.com>
2018-09-11 16:56:34 +0200

    This is where it all begins...
    Made function for error logger.

--- index.js
+++ index.js
@@ -6,62 +6,52 @@ var i2c = require('i2c');
 var address = 0x32;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
+function wireWrite(side) {
+    wire.write(side, (err) => {
+        if (err) {
+            console.log(err);    
+        }
+    });
+}
+
 // Rotating to the left
 Motor.prototype.Left = function() {
-    var Left = [7,3,0xa5,1,3,0xa5,2]; 
-    wire.write(Left, function(err) {
-        console.log(err);
-    });
-
+    var left = [7,3,0xa5,1,3,0xa5,2]; 
+    wireWrite(left);
 }
 
 // Rotating to the right
 Motor.prototype.Right = function() {
-    var Right = [7,3,0xa5,2,3,0xa5,1]; 
-    wire.write(Right, function(err) {
-        console.log(err);
-    });
-
+    var right = [7,3,0xa5,2,3,0xa5,1]; 
+    wireWrite(right);
 }
 
 // Driving to the left
 Motor.prototype.LeftDrive = function() {
-    var Left = [7,3,0x52,2,3,0xa5,2]; 
-    wire.write(Left, function(err) {
-        console.log(err);
-    });
-
+    var left = [7,3,0x52,2,3,0xa5,2]; 
+    wireWrite(left);
 }
 
 // Forwards
 Motor.prototype.Forward = function(direction) {
     direction += 100;
 
-    var Left = Math.round(255 - ((255 / 200) * direction));
-    var Right = 255 - Left;
-    var Forward = [7,3,Left,2,3,Right,2]; 
-    wire.write(Forward, function(err) {
-        console.log(err);
-    });   
-    
+    var left = Math.round(255 - ((255 / 200) * direction));
+    var right = 255 - left;
+    var forward = [7,3,left,2,3,right,2]; 
+    wireWrite(forward);
 }
 
 // Full power forwards
 Motor.prototype.Boost = function() {
-    var BoostForward = [7,3,0xFF,2,3,0xFF,2]; 
-    wire.write(BoostForward, function(err) {
-        console.log(err);
-    });   
-    
+    var boostForward = [7,3,0xFF,2,3,0xFF,2]; 
+    wireWrite(boostForward);
 }
 
 // Stopping
 Motor.prototype.Stop = function() {
-    var Stopping = [7,0,0,0,0,0,0]; 
-    wire.write(Stopping, function(err) {
-        console.log(err);
-    });      
-
+    var stopping = [7,0,0,0,0,0,0]; 
+    wireWrite(stopping);
 }
 
 module.exports = Motor;
