739791c0ef1fa9a8775f1963069cafd97aceb71c
Christiaan <christiaanvanarum2@gmail.com>
2018-09-11 13:17:53 +0200

    I'll explain when you're older!
    Motor control.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-
-
 function Motor() {
 
 }
+
+var i2c = require('i2c');
+var address = 0x32;
+var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+
+Motor.prototype.Left = function() {
+    console.log("Turning left");
+
+    var LSlow = [7,3,0xa5,1,3,0xa5,2]; 
+    wire.write(LSlow, function(err) {
+        console.log(err);
+    });
+
+}
+
+Motor.prototype.Forward = function() {
+    console.log("Going forward");
+
+    var MForward = [7,3,0xa5,2,3,0xa5,2]; 
+    wire.write(Mforward, function(err) {
+        console.log(err);
+    });   
+    
+}
+
+Motor.prototype.Stop = function() {
+    console.log("Stopping");
+
+    var Stopping = [7,0,0,0,0,0,0]; 
+    wire.write(Stopping, function(err) {
+        console.log(err);
+    });      
+    
+}
+
 
 module.exports = Motor;
