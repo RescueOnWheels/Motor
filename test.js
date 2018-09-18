bcab8969476e55ceb39022b732e7323341252774
Christiaan <christiaanvanarum2@gmail.com>
2018-09-18 13:47:08 +0200

    pep8 fixer
    Added backwards function and made a test script to test all directions.

--- test.js
+++ test.js
@@ -4,11 +4,21 @@ const wheel = new Motor();
 
 try {
   setTimeout(() => {
+    console.log("Going forwards");
     wheel.Forward(100);
     setTimeout(() => {
-      wheel.Stop();
-    }, 10000);
-  }, 5000);
+      console.log("Rotating to the right");
+      wheel.Right(100);
+      setTimeout(() => {
+        console.log("Rotating to the left");        
+        wheel.Left(100);
+        setTimeout(() => {
+          console.log("Going backwards");
+          wheel.Backwards(100);
+        }, 3000);
+      }, 3000);
+    }, 3000);
+  }, 3000);
 } catch (error) {
   wheel.Stop();
 }
