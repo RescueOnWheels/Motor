2a31f3161e0aba44ac05c3a64b60bd06dc1a08fa
Christiaan <christiaanvanarum2@gmail.com>
2018-09-18 15:19:42 +0200

    I did it for the lelz!
    Nino did this. I thank him a lot.. Thanks Nino
    Nino: "np"

--- test.js
+++ test.js
@@ -2,25 +2,37 @@ const Motor = require('./index.js');
 
 const wheel = new Motor();
 
-try {
-  setTimeout(() => {
-    console.log("Going forwards");
-    wheel.Forward(100);
-    setTimeout(() => {
-      console.log("Rotating to the right");
-      wheel.Right(100);
-      setTimeout(() => {
-        console.log("Rotating to the left");        
-        wheel.Left(100);
-        setTimeout(() => {
-          console.log("Going backwards");
-          wheel.Backwards(100);
-        }, 3000);
-      }, 3000);
-    }, 3000);
-  }, 3000);
-} catch (error) {
-  wheel.Stop();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
 }
 
-wheel.Stop();
+try {
+  console.log("Do this");
+  wheel.Left();
+  sleep(2000);
+
+  console.log("do more");
+  wheel.Right();
+  sleep(2000);
+
+  console.log("retreat");
+  wheel.Backwards();
+  sleep(2000);
+
+  console.log("hallo kutwereld<3");
+  wheel.Stop();
+
+} catch (error) {
+  console.log("Error i guess");
+  console.log(error);
+  wheel.Stop();
+
+} finally {
+  console.log("Finally stopping please");
+  wheel.Stop();
+} 
