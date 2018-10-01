8d7230bc2789cee6b1e8f181762a433aa65659d7
Martijn Vegter <mail@mvegter.me>
2018-10-01 11:25:24 +0200

    [ENHANCEMENT] I2C Mock interface (#13)

---
+++ test/constructor.test.js
@@ -0,0 +1,33 @@
+/**
+ * BDD / TDD assertion library.
+ */
+const chai = require('chai');
+
+/**
+ * Class to test.
+ */
+const motor = require('./../');
+
+chai.should();
+
+describe('constructor', () => {
+  it('should have access to the \'write_history\' property of wire', () => {
+    motor.wire.should.have.property('write_history');
+  });
+
+  it('should have access to the \'write_history\' property of wire, which should be an array', () => {
+    motor.wire.write_history.should.be.a('array');
+  });
+
+  it('should have access to the \'options\' property of wire', () => {
+    motor.wire.should.have.property('options');
+  });
+
+  it('should have access to the \'device\' property of wire.options', () => {
+    motor.wire.options.should.have.property('device');
+  });
+
+  it('should set the \'device\' property of wire.options to \'/dev/i2c-1\'', () => {
+    motor.wire.options.device.should.equal('/dev/i2c-1');
+  });
+});
