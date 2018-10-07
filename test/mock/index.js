06155c24d1ef7b235077cc0af7dad65daf47d4a0
Martijn Vegter <mail@mvegter.me>
2018-10-07 13:34:57 +0200

    Fixed bug in Forward

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,10 +1,12 @@
 const constructor = require('./constructor.test');
+const forward = require('./forward.test');
 const left = require('./left.test');
 const setGlobalPower = require('./setGlobalPower.test');
 const setSoftStart = require('./setSoftStart.test');
 
 describe('Mock', () => {
   describe('constructor', constructor);
+  describe('forward', forward);
   describe('left', left);
   describe('setGlobalPower', setGlobalPower);
   describe('setSoftStart', setSoftStart);
