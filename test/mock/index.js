2260232ec1e48443038344e2e3290753089a1fae
Martijn Vegter <mail@mvegter.me>
2018-10-07 10:09:37 +0200

    Added mock and real tests

---
+++ test/mock/index.js
@@ -0,0 +1,7 @@
+const constructor = require('./constructor.test');
+const left = require('./left.test');
+
+describe('Mock', () => {
+  describe('constructor', constructor);
+  describe('left', left);
+});
