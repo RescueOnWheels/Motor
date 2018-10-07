20ebb6646e90ca27d89cdf46e707cbe367a7cc8d
Martijn Vegter <mail@mvegter.me>
2018-10-07 10:40:40 +0200

    LOL! (#14)

---
+++ test/mock/index.js
@@ -0,0 +1,9 @@
+const constructor = require('./constructor.test');
+const left = require('./left.test');
+const setGlobalPower = require('./setGlobalPower.test');
+
+describe('Mock', () => {
+  describe('constructor', constructor);
+  describe('left', left);
+  describe('setGlobalPower', setGlobalPower);
+});
