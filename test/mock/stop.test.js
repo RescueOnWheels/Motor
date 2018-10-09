445dc6a28b67c671d6f164f3aeee8d8b7e9c4849
Christiaan van Arum <31652894+cremus@users.noreply.github.com>
2018-10-09 15:27:14 +0200

    that coulda been bad (#25)

---
+++ test/mock/stop.test.js
@@ -0,0 +1,42 @@
+/**
+ * BDD / TDD assertion library.
+ */
+const chai = require('chai');
+
+/**
+ * Class to test.
+ */
+const motor = require('./../../');
+
+chai.should();
+
+module.exports = () => {
+  it('should send a buffer with a length of \'7\'', () => {
+    // Act
+    motor.Stop();
+
+    // Assert
+    motor.wire.write_history[motor.wire.write_history.length - 1].should.have.lengthOf(7);
+  });
+
+  it('should send command \'7\'', () => {
+    // Act
+    motor.Stop();
+
+    // Assert
+    motor.wire.write_history[motor.wire.write_history.length - 1][0].should.equal(7);
+  });
+
+  it('should set indexes [1, 2, 3, 4, 5, 6] to \'0\'', () => {
+    // Act
+    motor.Stop();
+
+    // Assert
+    motor.wire.write_history[motor.wire.write_history.length - 1][1].should.equal(0);
+    motor.wire.write_history[motor.wire.write_history.length - 1][2].should.equal(0);
+    motor.wire.write_history[motor.wire.write_history.length - 1][3].should.equal(0);
+    motor.wire.write_history[motor.wire.write_history.length - 1][4].should.equal(0);
+    motor.wire.write_history[motor.wire.write_history.length - 1][5].should.equal(0);
+    motor.wire.write_history[motor.wire.write_history.length - 1][6].should.equal(0);
+  });
+};
