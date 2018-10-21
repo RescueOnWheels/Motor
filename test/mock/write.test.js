b6002e35b6554fd8d25c7a2754a5c403b29c2d3b
Martijn Vegter <mail@mvegter.me>
2018-10-21 11:40:52 +0200

    Throw error for unknown command

--- test/mock/write.test.js
+++ test/mock/write.test.js
@@ -4,49 +4,70 @@ const chai = require('chai');
 /* Test target */
 const motor = require('./../../');
 
-chai.should();
+const should = chai.should();
 
 module.exports = () => {
-  beforeEach(() => {
-    motor.wire.write_history = [];
+  describe('duplicate commands', () => {
+    beforeEach(() => {
+      motor.wire.write_history = [];
+    });
+
+    it('should prevent duplicate commands', () => {
+      // Act
+      motor.Stop();
+      motor.Stop();
+
+      // Assert
+      motor.wire.write_history.length.should.equal(1);
+    });
+
+
+    it('should prevent duplicate commands, even if there is another function called in between.', () => {
+      // Act
+      motor.Stop();
+      motor.setGlobalPower(10);
+      motor.Stop();
+
+      // Assert
+      motor.wire.write_history.length.should.equal(2);
+    });
+
+    it('should not prevent commands, if there command is the same but values different.', () => {
+      // Act
+      motor.move({ speed: 10 });
+      motor.move({ speed: 20 });
+
+      // Assert
+      motor.wire.write_history.length.should.equal(2);
+    });
+
+    it('should not prevent commands, if an older command is called again.', () => {
+      // Act
+      motor.move({ speed: 10 });
+      motor.move({ speed: 20 });
+      motor.move({ speed: 10 });
+
+      // Assert
+      motor.wire.write_history.length.should.equal(3);
+    });
   });
 
-  it('should prevent duplicate commands', () => {
-    // Act
-    motor.Stop();
-    motor.Stop();
+  describe('(un)known commands', () => {
+    it('should throw an error for an unknown command', () => {
+      // Arrange
+      const command = [1, 2, 3, 4];
 
-    // Assert
-    motor.wire.write_history.length.should.equal(1);
-  });
+      try {
+        // Act
+        motor.write(command);
 
-
-  it('should prevent duplicate commands, even if there is another function called in between.', () => {
-    // Act
-    motor.Stop();
-    motor.setGlobalPower(10);
-    motor.Stop();
-
-    // Assert
-    motor.wire.write_history.length.should.equal(2);
-  });
-
-  it('should not prevent commands, if there command is the same but values different.', () => {
-    // Act
-    motor.move({ speed: 10 });
-    motor.move({ speed: 20 });
-
-    // Assert
-    motor.wire.write_history.length.should.equal(2);
-  });
-
-  it('should not prevent commands, if an older command is called again.', () => {
-    // Act
-    motor.move({ speed: 10 });
-    motor.move({ speed: 20 });
-    motor.move({ speed: 10 });
-
-    // Assert
-    motor.wire.write_history.length.should.equal(3);
+        // Assert
+        should.fail('No error was thrown when it should have been!');
+      } catch (err) {
+        // Assert
+        err.should.be.an.instanceOf(Error);
+        err.should.have.property('message', 'Unknown command \'1\' with args: \'2,3,4\'');
+      }
+    });
   });
 };
