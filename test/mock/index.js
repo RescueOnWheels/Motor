7770be345673d9cf53894ac44e154bcd5ad0051d
Martijn Vegter <mail@mvegter.me>
2018-10-29 10:50:21 +0100

    copy and paste is not a design pattern . (#33)

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,13 +1,36 @@
+/* Motor */
 const constructor = require('./constructor.test');
-const movement = require('./movement.test');
-const left = require('./left.test');
+const move = require('./move.test');
 const setGlobalPower = require('./setGlobalPower.test');
 const setSoftStart = require('./setSoftStart.test');
+const stop = require('./stop.test');
+const turnLeft = require('./turnLeft.test');
+const turnRight = require('./turnRight.test');
+const write = require('./write.test');
+
+/* Helpers */
+const arraysEqual = require('./helpers/arraysEqual.test');
+
+/* Interfaces */
+const i2c = require('./interfaces/i2c.mock.test');
 
 describe('Mock', () => {
-  describe('constructor', constructor);
-  describe('movement', movement);
-  describe('left', left);
-  describe('setGlobalPower', setGlobalPower);
-  describe('setSoftStart', setSoftStart);
+  describe('helpers', () => {
+    describe('arraysEqual', arraysEqual);
+  });
+
+  describe('interfaces', () => {
+    describe('i2c.mock', i2c);
+  });
+
+  describe('motor', () => {
+    describe('constructor', constructor);
+    describe('move', move);
+    describe('setGlobalPower', setGlobalPower);
+    describe('setSoftStart', setSoftStart);
+    describe('stop', stop);
+    describe('turnLeft', turnLeft);
+    describe('turnRight', turnRight);
+    describe('write', write);
+  });
 });
---
+++ test/mock/write.test.js
@@ -0,0 +1,67 @@
+/* Packages */
+const chai = require('chai');
+
+/* Test target */
+const motor = require('./../../');
+
+chai.should();
+
+module.exports = () => {
+  beforeEach(() => {
+    motor.wire.write_history = [];
+  });
+
+  describe('duplicate commands', () => {
+    it('should prevent duplicate commands', () => {
+      // Act
+      motor.stop();
+      motor.stop();
+
+      // Assert
+      motor.wire.write_history.length.should.equal(1);
+    });
+
+    it('should prevent duplicate commands, even if there is another function called in between.', () => {
+      // Act
+      motor.stop();
+      motor.setGlobalPower(10);
+      motor.stop();
+
+      // Assert
+      motor.wire.write_history.length.should.equal(2);
+    });
+
+    it('should not prevent commands, if there command is the same but values different.', () => {
+      // Act
+      motor.move({ speed: 10 });
+      motor.move({ speed: 20 });
+
+      // Assert
+      motor.wire.write_history.length.should.equal(2);
+    });
+
+    it('should not prevent commands, if an older command is called again.', () => {
+      // Act
+      motor.move({ speed: 10 });
+      motor.move({ speed: 20 });
+      motor.move({ speed: 10 });
+
+      // Assert
+      motor.wire.write_history.length.should.equal(3);
+    });
+  });
+
+  describe('(un)known commands', () => {
+    it('should send stop command if there is an error (e.g. an unknown command)', () => {
+      // Arrange
+      const command = [1, 2, 3, 4];
+
+      // Act
+      motor.write(command);
+
+      // Assert
+      motor.wire.write_history.length.should.equal(2);
+      motor.wire.write_history[1].should.deep.equal([7, 0, 0, 0, 0, 0, 0]);
+    });
+  });
+};
