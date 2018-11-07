f30de8acf853dd6fcb7ad13f994289ec72518cd8
Martijn Vegter <mail@mvegter.me>
2018-11-07 11:21:00 +0100

    Still can't get this right...

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 let toMockOrNotToMockThatIsTheQuestion;
+
+/* istanbul ignore else */
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'CI') {
   // eslint-disable-line no-console
   console.warn('Motor: Using the mock tests!'); // eslint-disable-line no-console
